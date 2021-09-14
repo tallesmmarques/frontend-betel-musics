96b719ab31b5c724d86aeb4364d90ea45bcf78c1
Talles Marques <tallesemanuel1@gmail.com>
2021-09-13 21:42:40 -0300

    Layout e tabela geral

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import {useEffect, useState} from "react"
+import {Box, ChakraProvider, Text, Flex, Heading, Table, Thead, Tbody, Tr, Th, Button, Link, CircularProgress} from "@chakra-ui/react"
+import {AddIcon, ExternalLinkIcon} from "@chakra-ui/icons"
 import api from "./service/api"
 
 function App() {
@@ -27,12 +29,9 @@ function App() {
       .catch(err => console.error(err))
   }, [])
 
-  useEffect(() => {
-    console.log(musics)
-  }, [musics])
   // useEffect(() => {
-  //   console.log(updating)
-  // }, [updating])
+  //   console.log(musics)
+  // }, [musics])
 
   const handleUpdate = (event, music) => {
     setUpdating(past => ({on: true, music}))
@@ -54,32 +53,85 @@ function App() {
   }
 
   if(loading) {
-    return <div>
-      <h1>Loading...</h1>
-    </div>
+    return <ChakraProvider>
+      <Flex bg="#f3f4f5" minH="100vh" align="center" justify="center">
+        <CircularProgress isIndeterminate size="50px" />
+      </Flex>
+    </ChakraProvider>
   }
 
   return (
-    <div>
-      <h1>Hello World</h1>
-      {musics.map(music => (
-        <div key={music.id}>
-          <p>{music?.name} - {music?.author}</p>
-          <button name={music.id} onClick={(e) => handleUpdate(e, music)} disabled={updating.on}>Atualizar</button>
-        </div>
-      ))}
+    <ChakraProvider>
+      <Flex bg="#f3f4f5" minH="100vh" flexDirection="column">
 
-      {updating.on ? (<div>
-        <h2>Atualizando {updating.music.name}</h2>
-        {["name", "author", "linkCifra", "linkYoutube"].map(field => (
-          <input key={field} type="text" name={field} value={updating.music[field]} onChange={handleInput} />
-        ))}
+        <Box h="5px" bg="blue.500"></Box>
 
-        <button onClick={(e) => handleSave(e, "cancelar", updating.music)}>Cancelar</button>
-        <button onClick={(e) => handleSave(e, "save", updating.music)}>Salvar</button>
-      </div>
-      ) : <></>}
-    </div>
+        <Box p="50px" bg="white" boxShadow="sm">
+          <Text pb="5px" color="blackAlpha.500">Coletânia de Músicas</Text>
+          <Heading>Betel Musics</Heading>
+        </Box>
+
+        <Box p="50px" px="100px">
+          <Flex pb="20px" align="center" >
+            <Heading flex="1" size="md">Todas Músicas</Heading>
+            <Button colorScheme="blue" variant="outline" maxW="150px"><AddIcon mr="10px"/> Criar Música</Button>
+          </Flex>
+          <Table variant="simple" bg="white" boxShadow="sm">
+            <Thead>
+              <Tr>
+                {["Música", "Artista", "Gênero", "Alber", "Lucimeire", "Adolescentes", "CifraClub", "Youtube"].map(field => (
+                  <Th key={field} pt="15px" fontSize="11px" fontWeight="bold" isTruncated>{field}</Th>
+                ))}
+              </Tr>
+            </Thead>
+            <Tbody>
+              {musics.map(music => {
+                const tomAlber = music.ministeriosInfo.find(mi => mi.ministerio === "sdn-alber")?.tom
+                const tomLucimeire = music.ministeriosInfo.find(mi => mi.ministerio === "sdn-lucimeire")?.tom
+                const tomAdolescentes = music.ministeriosInfo.find(mi => mi.ministerio === "adolescentes")?.tom
+                return (
+                  <Tr key={music.id}>
+                    <Th textTransform="none" fontSize="14px" minW="200px" fontWeight="light" isTruncated>{music.name}</Th>
+                    <Th textTransform="none" fontSize="14px" minW="200px" fontWeight="light" isTruncated>{music.author}</Th>
+                    <Th textTransform="none" fontSize="14px" minW="100px" bg="gray.50" fontWeight="light" isTruncated>Adoração</Th>
+                    {/* {[music.author, "Adoração"].map(field => (
+                      <Th key={field} textTransform="none" maxW="200px" fontSize="14px" fontWeight="light" isTruncated>{field}</Th>
+                    ))} */}
+                    {[tomAlber, tomLucimeire, tomAdolescentes].map(field => (
+                      <Th key={field} textTransform="none" maxW="50px" fontSize="14px" fontWeight="light">{field}</Th>
+                    ))}
+                    {[music.linkCifra, music.linkYoutube].map(field => (
+                      <Th key={field} textTransform="none" fontSize="14px" fontWeight="light" textOverflow="ellipsis">
+                        <Link href={field} isExternal>Acessar Link <ExternalLinkIcon mx="2px" mb="2px"/></Link>
+                      </Th>
+                    ))}
+                  </Tr>
+                )
+              })}
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </ChakraProvider>
+    // <div>
+    //   {musics.map(music => (
+    //     <div key={music.id}>
+    //       <p>{music?.name} - {music?.author}</p>
+    //       <button name={music.id} onClick={(e) => handleUpdate(e, music)} disabled={updating.on}>Atualizar</button>
+    //     </div>
+    //   ))}
+
+    //   {updating.on ? (<div>
+    //     <h2>Atualizando {updating.music.name}</h2>
+    //     {["name", "author", "linkCifra", "linkYoutube"].map(field => (
+    //       <input key={field} type="text" name={field} value={updating.music[field]} onChange={handleInput} />
+    //     ))}
+
+    //     <button onClick={(e) => handleSave(e, "cancelar", updating.music)}>Cancelar</button>
+    //     <button onClick={(e) => handleSave(e, "save", updating.music)}>Salvar</button>
+    //   </div>
+    //   ) : <></>}
+    // </div>
   );
 }
 
